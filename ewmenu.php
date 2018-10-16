2b1383966bb523994027020605af870796ad87dd
ACER E1-451G <apps.selusin@gmail.com>
2018-10-17 00:27:26 +0700

    .

--- ewmenu.php
+++ ewmenu.php
@@ -7,7 +7,6 @@ $RootMenu->IsRoot = TRUE;
 $RootMenu->AddMenuItem(11, "mi_cf01_home_php", $Language->MenuPhrase("11", "MenuText"), "cf01_home.php", -1, "", AllowListMenu('{51CA4EA8-8F8C-4E6D-9D3C-6714DAAEE6FC}cf01_home.php'), FALSE, TRUE);
 $RootMenu->AddMenuItem(3, "mi_t03_pinjaman", $Language->MenuPhrase("3", "MenuText"), "t03_pinjamanlist.php", -1, "", AllowListMenu('{51CA4EA8-8F8C-4E6D-9D3C-6714DAAEE6FC}t03_pinjaman'), FALSE, FALSE);
 $RootMenu->AddMenuItem(12, "mci_Setup", $Language->MenuPhrase("12", "MenuText"), "", -1, "", IsLoggedIn(), FALSE, TRUE);
-$RootMenu->AddMenuItem(1, "mi_t01_nasabah", $Language->MenuPhrase("1", "MenuText"), "t01_nasabahlist.php", 12, "", AllowListMenu('{51CA4EA8-8F8C-4E6D-9D3C-6714DAAEE6FC}t01_nasabah'), FALSE, FALSE);
 $RootMenu->AddMenuItem(7, "mi_t96_employees", $Language->MenuPhrase("7", "MenuText"), "t96_employeeslist.php", 12, "", AllowListMenu('{51CA4EA8-8F8C-4E6D-9D3C-6714DAAEE6FC}t96_employees'), FALSE, FALSE);
 $RootMenu->AddMenuItem(8, "mi_t97_userlevels", $Language->MenuPhrase("8", "MenuText"), "t97_userlevelslist.php", 12, "", (@$_SESSION[EW_SESSION_USER_LEVEL] & EW_ALLOW_ADMIN) == EW_ALLOW_ADMIN, FALSE, FALSE);
 $RootMenu->AddMenuItem(10011, "mci_List", $Language->MenuPhrase("10011", "MenuText"), "", -1, "", IsLoggedIn(), FALSE, TRUE);
--- ewmobilemenu.php
+++ ewmobilemenu.php
@@ -6,7 +6,6 @@ $RootMenu->IsRoot = TRUE;
 $RootMenu->AddMenuItem(11, "mmi_cf01_home_php", $Language->MenuPhrase("11", "MenuText"), "cf01_home.php", -1, "", AllowListMenu('{51CA4EA8-8F8C-4E6D-9D3C-6714DAAEE6FC}cf01_home.php'), FALSE, TRUE);
 $RootMenu->AddMenuItem(3, "mmi_t03_pinjaman", $Language->MenuPhrase("3", "MenuText"), "t03_pinjamanlist.php", -1, "", AllowListMenu('{51CA4EA8-8F8C-4E6D-9D3C-6714DAAEE6FC}t03_pinjaman'), FALSE, FALSE);
 $RootMenu->AddMenuItem(12, "mmci_Setup", $Language->MenuPhrase("12", "MenuText"), "", -1, "", IsLoggedIn(), FALSE, TRUE);
-$RootMenu->AddMenuItem(1, "mmi_t01_nasabah", $Language->MenuPhrase("1", "MenuText"), "t01_nasabahlist.php", 12, "", AllowListMenu('{51CA4EA8-8F8C-4E6D-9D3C-6714DAAEE6FC}t01_nasabah'), FALSE, FALSE);
 $RootMenu->AddMenuItem(7, "mmi_t96_employees", $Language->MenuPhrase("7", "MenuText"), "t96_employeeslist.php", 12, "", AllowListMenu('{51CA4EA8-8F8C-4E6D-9D3C-6714DAAEE6FC}t96_employees'), FALSE, FALSE);
 $RootMenu->AddMenuItem(8, "mmi_t97_userlevels", $Language->MenuPhrase("8", "MenuText"), "t97_userlevelslist.php", 12, "", (@$_SESSION[EW_SESSION_USER_LEVEL] & EW_ALLOW_ADMIN) == EW_ALLOW_ADMIN, FALSE, FALSE);
 $RootMenu->AddMenuItem(10011, "mmci_List", $Language->MenuPhrase("10011", "MenuText"), "", -1, "", IsLoggedIn(), FALSE, TRUE);
